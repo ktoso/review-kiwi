d1bd36331a47efdd1f0b263886bae2796eeb60de
Konrad Malawski <konrad.malawski@project13.pl>
2012-10-03 01:59:07 +0200

    dont send the same one twice

--- repo-worker/src/main/scala/com/reviewkiwi/repoworker/fetch/ChangeToFetchQueueFetcherActor.scala
+++ repo-worker/src/main/scala/com/reviewkiwi/repoworker/fetch/ChangeToFetchQueueFetcherActor.scala
@@ -2,7 +2,7 @@ package com.reviewkiwi.repoworker.fetch
 
 import akka.actor._
 import akka.actor.{ActorRef, Actor}
-import com.reviewkiwi.model.{KiwiRepository, ChangeToFetch}
+import com.reviewkiwi.model.{ChangeFetched, KiwiRepository, ChangeToFetch}
 import com.reviewkiwi.repoworker.data.FetchThisChange
 import java.net.URI
 import com.weiglewilczek.slf4s.Logging
@@ -17,7 +17,13 @@ class ChangeToFetchQueueFetcherActor(changesFetcher: ActorRef) extends Actor wit
       ChangeToFetch.findAll foreach { fetchMe =>
         logger.info("Will try to fetch changes for [%s][%s] ".format(fetchMe.repo.is, fetchMe.id.is))
 
-        changesFetcher ! FetchThisChange(new URI(fetchMe.repo.obj.map(_.fetchUrl.is).get), fetchMe.objectId.is)
+        if(alreadyFetched(fetchMe)) fetchMe.delete_!
+        else changesFetcher ! FetchThisChange(new URI(fetchMe.repo.obj.map(_.fetchUrl.is).get), fetchMe.objectId.is)
       }
+  }
+
+  def alreadyFetched(change: ChangeToFetch): Boolean = {
+    import com.foursquare.rogue.Rogue._
+    (ChangeFetched where(_.objectId eqs change.objectId.is) count()) > 0 // todo improve me, should take repo name too
   }
 }
