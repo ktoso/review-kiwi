00c06a8532d05889f33bfcf61771c02ba9ba563e
Konrad Malawski <konrad.malawski@project13.pl>
2013-11-01 17:46:41 +0000

    hacky register

---
+++ repo-worker/src/main/scala/com/reviewkiwi/repoworker/RegisterRunner.scala
@@ -0,0 +1,29 @@
+package com.reviewkiwi.repoworker
+
+import akka.actor._
+import Actor._
+import data.{FetchNewChangesFromReposEach, FetchNewChangesFrom, FetchThisChange}
+import fetch.{ChangeToFetchQueueFetcherActor, ChangesFetcherActor}
+import git.{GitDiffer, FreshCommitsExtractor, GitCloner}
+import notify.cli.CliNotifierActor
+import java.net.URI
+import notify.email.{EmailSenderActor, EmailNotifierActor}
+import notify.template.html.LineByLineDiffEmailHtml
+import com.reviewkiwi.common.email.EmailSender
+import com.reviewkiwi.repoworker.fetcher.CheckQueue
+import akka.util.duration._
+import com.reviewkiwi.model.mongo.{Config, MongoInit}
+import com.reviewkiwi.model._
+
+object RegisterRunner extends App {
+
+  val config = Config.readFromProperties()
+  MongoInit.init(config)
+
+  KiwiUser.createRecord
+    .name(readLine("name: "))
+    .primaryEmail(readLine("email: "))
+    .oauthToken(readLine("oauth token: "))
+    .save()
+
+}
