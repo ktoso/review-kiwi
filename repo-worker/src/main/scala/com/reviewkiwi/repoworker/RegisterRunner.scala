5ddc9e50ae5dab1dba14d8cfc752bbe55b4e7f72
Konrad Malawski <konrad.malawski@project13.pl>
2013-11-04 09:46:41 +0000

    fix...

--- repo-worker/src/main/scala/com/reviewkiwi/repoworker/RegisterRunner.scala
+++ repo-worker/src/main/scala/com/reviewkiwi/repoworker/RegisterRunner.scala
@@ -40,7 +40,6 @@ object RegisterRunner extends App {
     user
       .repos(repo.name.get :: user.repos.get)
       .watchedRepos(repo.name.get :: user.repos.get)
-      watchers()
       .save(true)
   }
 
